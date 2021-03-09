b14889c9b0ffe1ecea1769a14f89cb09cbd6f455
Daniel Nichols <dando18studios@gmail.com>
2021-03-08 23:19:59 -0500

    added functionality to call and read profiles from gprof

--- profiler.py
+++ profiler.py
@@ -6,13 +6,13 @@ from sh import Command
 import os
 from util import vprint
 
-def get_profiler(profiler_settings):
+def get_profiler(profiler_settings, verbose):
     profiler_name = profiler_settings['profiler']
 
     if profiler_name == 'gprof':
-        return GProfProfiler(profiler_settings)
+        return GProfProfiler(profiler_settings, verbose=verbose)
     elif profiler_name == 'hpctoolkit':
-        return HPCToolkitProfiler(profiler_settings)
+        return HPCToolkitProfiler(profiler_settings, verbose=verbose)
     else:
         print('Unknown profiler type')
         return None
@@ -32,16 +32,36 @@ class GProfProfiler(Profiler):
     def profile(self, repo):
         # for gprof we can run each test in the application
         # however, we must move and/or rename gman.out each time
-        # and then we should go ahead and
+        # and then we should go ahead and read hatchet profile
+        profiles = {}
         for test in repo.itertests():
-            vprint(args.verbose, 'Profiling test \'{}\'...'.format(test['name']))
-            cmd = Command(os.path.join(test['prefix'], test['executable']))
+            vprint(self.verbose, 'Profiling test \'{}\'...'.format(test['name']))
+            exec_path = os.path.join(test['prefix'], test['executable'])
+            cmd = Command(exec_path)
 
             try:
                 cmd(test['args'])
             except:
                 vprint(self.verbose, 'Running test \'{}\' failed...'.format(test['name']))
+                continue
 
+            if not os.path.isfile('gmon.out'):
+                vprint(self.verbose, 'Unable to read profile...')
+                continue
+
+            # read in profile with gprof and gprof2dot
+            dotfile_name = 'profile-dot-graph.dot'
+            gprof = Command('gprof')
+            gprof2dot = Command('gprof2dot')
+            with open(dotfile_name, 'w+') as outFile:
+                gprof2dot(gprof(exec_path), '-n0', '-e0', _out=outFile)
+
+            # finally read this into hatchet
+            gf = ht.GraphFrame.from_gprof_dot(dotfile_name)
+
+            profiles[test['name']] = gf
+
+        return profiles
 
 
 class HPCToolkitProfiler(Profiler):
